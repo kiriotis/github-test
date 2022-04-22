f8795017a4d73f1039418c06c9ead5f7b715f4dd
ivan <ivan@DESKTOP-NELLSAH>
2022-04-22 19:47:37 +0300

    kek

--- github test/Program.cs
+++ github test/Program.cs
@@ -7,7 +7,7 @@ namespace github_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(" ну типо да");
+            Console.WriteLine(" ну типо ок");
         }
     }
 }
